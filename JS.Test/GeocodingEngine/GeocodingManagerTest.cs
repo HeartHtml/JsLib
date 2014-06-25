89bbc0bbb52a070d4662f3dae1494d5967928649
horheyortega <horhey.ortega@gmail.com>
2014-06-25 19:30:18 -0400

    Finishing entity setup

---
+++ JS.Test/GeocodingEngine/GeocodingManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JS.Business.GeocodingEngine;
+using JS.Entities.GeocodingEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JS.Test.GeocodingEngine
+{
+    [TestClass]
+    public class GeocodingManagerTest
+    {
+        [TestMethod]
+        public void GeocodingTest()
+        {
+            GeocodeObject obj = GeocodeManager.Geocode("704 NW 128th PL Miami FL 33182");
+
+            Assert.IsNotNull(obj);
+
+            Assert.IsTrue(obj.IsValid);
+        }
+    }
+}
